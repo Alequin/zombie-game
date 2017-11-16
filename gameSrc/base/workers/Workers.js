c8b897b0785bc234db0cded6772b3a16551eb150
James Cox <j-cox03@hotmail.com>
2017-11-16 13:25:23 +0000

    Workers: made method to calc effort of all workers

--- gameSrc/base/workers/Workers.js
+++ gameSrc/base/workers/Workers.js
@@ -5,6 +5,14 @@ class Workers{
     this.workers = []
   }
 
+  getTotalEffort(){
+    let count = 0
+    for(let worker of this.workers){
+      count += worker.getEffort()
+    }
+    return count
+  }
+
 }
 
 export default Workers
--- gameSrc/specs/WorkersSpecs.js
+++ gameSrc/specs/WorkersSpecs.js
@@ -1,4 +1,5 @@
 import assert from "assert"
+import Worker from "./../base/workers/Worker.js"
 import Workers from "./../base/workers/Workers.js"
 
 describe("Workers", function(){
@@ -19,5 +20,15 @@ describe("Workers", function(){
     assert.deepEqual(result, expected)
   })
 
+  it("can get total effort of all workers", () => {
+    workers.workers = [
+      new Worker(1),
+      new Worker(1),
+      new Worker(1),
+    ]
 
+    const expected = 30
+    const result = workers.getTotalEffort()
+    assert.strictEqual(result, expected)
+  })
 })
