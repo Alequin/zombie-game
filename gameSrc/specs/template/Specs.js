7a92a40e656da1b2b8de9d565ab3b2ffc99fbc09
James Cox <j-cox03@hotmail.com>
2017-11-16 12:42:49 +0000

    Made template for test files

---
+++ gameSrc/specs/template/Specs.js
@@ -0,0 +1,14 @@
+// // https://nodejs.org/api/assert.html
+// import assert from "assert"
+//
+// describe("test", function(){
+//
+//   beforeEach(() => {
+//
+//   })
+//
+//   it("do stuff", () => {
+//
+//   })
+//
+// })
