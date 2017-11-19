2044fb229383fc96755c89d8710e3d13a31f1fba
James Cox <j-cox03@hotmail.com>
2017-11-19 12:56:04 +0000

    buildingGroup: made methods count, add, remove

--- gameSrc/base/buildings/BuildingGroup.js
+++ gameSrc/base/buildings/BuildingGroup.js
@@ -1,7 +1,20 @@
 
 class BuildingGroup{
-  constructor(){
+  constructor(singleBuildingSize){
+    this._count = 0
+    this._singleBuildingSize = singleBuildingSize
+  }
 
+  count(){
+    return this._count
+  }
+
+  add(amount){
+    this._count += amount
+  }
+
+  remove(amount){
+    this._count -= amount
   }
 }
 
--- gameSrc/specs/BuidlingGroupSpecs.js
+++ gameSrc/specs/BuidlingGroupSpecs.js
@@ -6,11 +6,35 @@ describe("Building Group - basic", function(){
   let buildingGroup
 
   beforeEach(() => {
-    buildingGroup = new BuildingGroup()
+    buildingGroup = new BuildingGroup(2)
   })
 
   it("can initialise buildingGroup", () => {
     assert.ok(buildingGroup)
+    assert.strictEqual(buildingGroup._singleBuildingSize, 2)
+  })
+
+  it("can count number of buildings", () => {
+    let expected = 0
+    let result = buildingGroup.count()
+    assert.strictEqual(result, expected)
+  })
+
+  it("can add to number of buildings", () => {
+    buildingGroup.add(10)
+
+    let expected = 10
+    let result = buildingGroup.count()
+    assert.strictEqual(result, expected)
+  })
+
+  it("can remove buildings from total count", () => {
+    buildingGroup.add(10)
+    buildingGroup.remove(5)
+
+    let expected = 5
+    let result = buildingGroup.count()
+    assert.strictEqual(result, expected)
   })
 
 })
