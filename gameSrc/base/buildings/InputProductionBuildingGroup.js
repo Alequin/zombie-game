ae6a1b39e1d4e7db028500a25189eb5c293eb582
James Cox <j-cox03@hotmail.com>
2017-11-19 15:08:09 +0000

    InputProdcutionBuildingGroup: made method calcRequiredInput

--- gameSrc/base/buildings/InputProductionBuildingGroup.js
+++ gameSrc/base/buildings/InputProductionBuildingGroup.js
@@ -12,6 +12,10 @@ class InputProductionBuildingGroup extends ProductionBuildingGroup{
     this._productionRatio = productionRatio
   }
 
+  calcRequiredInput(){
+    const maxOutput = this.calcProduction()
+    return maxOutput * this._productionRatio
+  }
 }
 
 export default InputProductionBuildingGroup
--- gameSrc/specs/InputProductionBuildingGroupSpecs.js
+++ gameSrc/specs/InputProductionBuildingGroupSpecs.js
@@ -6,14 +6,23 @@ describe("Input Production Building Group - basic", function(){
   let inputProductionBuildingGroup
 
   beforeEach(() => {
-    inputProductionBuildingGroup = new InputProductionBuildingGroup(0.3, 1, 10, 2)
+    inputProductionBuildingGroup = new InputProductionBuildingGroup(3, 3, 10, 2)
   })
 
   it("can initialise inputProductionBuildingGroup", () => {
     assert.ok(inputProductionBuildingGroup)
-    assert.strictEqual(inputProductionBuildingGroup._productionRatio, 0.3)
-    assert.strictEqual(inputProductionBuildingGroup._productionPerEffort, 1)
+    assert.strictEqual(inputProductionBuildingGroup._productionRatio, 3)
+    assert.strictEqual(inputProductionBuildingGroup._productionPerEffort, 3)
     assert.strictEqual(inputProductionBuildingGroup._singleBuildingEffortCapacity, 10)
     assert.strictEqual(inputProductionBuildingGroup._singleBuildingSize, 2)
+  })
+
+  it("can calculate required input", () => {
+    inputProductionBuildingGroup.add(10)
+    inputProductionBuildingGroup.addEffort(50)
+
+    let expected = 450
+    let result = inputProductionBuildingGroup.calcRequiredInput()
+    assert.strictEqual(result, expected)
   })
 })
