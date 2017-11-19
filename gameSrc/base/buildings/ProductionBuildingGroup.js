30e404509c3de3dac243d21f6a5de79f7ab7debe
James Cox <j-cox03@hotmail.com>
2017-11-19 13:56:04 +0000

    ProductionBuildingGroup: made methods to manage effort

--- gameSrc/base/buildings/ProductionBuildingGroup.js
+++ gameSrc/base/buildings/ProductionBuildingGroup.js
@@ -4,8 +4,28 @@ class ProductionBuildingGroup extends buildingGroup{
   constructor(productionPerEffort, singleBuildingEffortCapacity, singleBuildingSize){
     super(singleBuildingSize)
     this._productionPerEffort = productionPerEffort
-    this._currentEffort = 0
+    this._effort = 0
     this._singleBuildingEffortCapacity = singleBuildingEffortCapacity
+  }
+
+  effortCapacity(){
+    return this._singleBuildingEffortCapacity * this._count
+  }
+
+  currentEffort(){
+    return this._effort
+  }
+
+  addEffort(amount){
+    if(amount < 0) throw new Error("Amount cannot be negative")
+    this._effort += amount
+  }
+
+  removeEffort(amount){
+    if(amount < 0) throw new Error("Amount cannot be negative")
+    const result = this._effort - amount
+    if(result < 0) throw new Error("Cannot remove more effort than is within the building")
+    this._effort = result
   }
 }
 
--- gameSrc/specs/ProductionBuildingGroupSpecs.js
+++ gameSrc/specs/ProductionBuildingGroupSpecs.js
@@ -16,4 +16,66 @@ describe("Building Group - basic", function(){
     assert.strictEqual(productionBuildingGroup._singleBuildingSize, 2)
   })
 
+  it("can get effort capacity", () => {
+    productionBuildingGroup.add(10)
+
+    let expected = 100
+    let result = productionBuildingGroup.effortCapacity()
+    assert.strictEqual(result, expected)
+  })
+
+  it("can get current effort", () => {
+    let expected = 0
+    let result = productionBuildingGroup.currentEffort()
+    assert.strictEqual(result, expected)
+  })
+
+  it("can add and remove effort", () => {
+    productionBuildingGroup.addEffort(10)
+
+    let expected = 10
+    let result = productionBuildingGroup.currentEffort()
+    assert.strictEqual(result, expected)
+
+    productionBuildingGroup.removeEffort(5)
+
+    expected = 5
+    result = productionBuildingGroup.currentEffort()
+    assert.strictEqual(result, expected)
+  })
+
+  it("cannot use negative values to modify the effort", () => {
+    assert.throws(() => {
+      productionBuildingGroup.addEffort(-1)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.addEffort(-100)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.addEffort(-10000)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(-1)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(-100)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(-10000)
+    })
+  })
+
+  it("cannot remove more effort than exists", () => {
+    productionBuildingGroup.addEffort(10)
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(11)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(100)
+    })
+    assert.throws(() => {
+      productionBuildingGroup.removeEffort(10000)
+    })
+  })
+
 })
