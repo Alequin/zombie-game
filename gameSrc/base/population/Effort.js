f631d2b7e0a79684e64f18f35e45c31744367419
James Cox <j-cox03@hotmail.com>
2017-11-19 13:03:54 +0000

    Effort: changed the point at which properties in use and remove methods are modified

--- gameSrc/base/population/Effort.js
+++ gameSrc/base/population/Effort.js
@@ -34,18 +34,20 @@ class Effort{
 
   use(amount){
     if(amount <= 0) throw new Error("Effort used cannot be less than or equal to 0")
-    this._usedThisTurn += amount
-    if(this._usedThisTurn > this._totalThisTurn){
+    const result = this._usedThisTurn + amount
+    if(result > this._totalThisTurn){
       throw new Error("Effort used cannot be greater than the current max")
     }
+    this._usedThisTurn = result
   }
 
   return(amount){
     if(amount <= 0) throw new Error("Effort returned cannot be less than or equal to 0")
-    this._usedThisTurn-=amount
-    if(this._usedThisTurn < 0){
+    const result = this._usedThisTurn - amount
+    if(result < 0){
       throw new Error("Returned effort cannot be greater than the used effort")
     }
+    this._usedThisTurn = result
   }
 }
 
