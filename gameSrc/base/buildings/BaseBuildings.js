acbc227de1b5f9eada693c77b61c056beb0b787a
James Cox <j-cox03@hotmail.com>
2017-11-19 16:39:08 +0000

    BaseBuildings: added exceptions to esure correct object is used

--- gameSrc/base/buildings/BaseBuildings.js
+++ gameSrc/base/buildings/BaseBuildings.js
@@ -1,9 +1,18 @@
 
 class BaseBuildings{
-  constructor(houses, farms, lumbarYards, scrapYards){
+  constructor(houses, farms, lumberYards, scrapYards){
+    if(houses.constructor.name != "Houses")
+      throw new Error("houses must be a Houses object")
+    if(farms.constructor.name != "ProductionBuildingGroup")
+      throw new Error("farms must be a ProductionBuildingGroup object")
+    if(lumberYards.constructor.name != "InputProductionBuildingGroup")
+      throw new Error("lumberYards must be a inputProductionBuildingGroup object")
+    if(scrapYards.constructor.name != "InputProductionBuildingGroup")
+      throw new Error("scrapYards must be a inputProductionBuildingGroup object")
+
     this.houses = houses
     this.farms = farms
-    this.lumbarYards = lumbarYards
+    this.lumberYards = lumberYards
     this.scrapYards = scrapYards
   }
 
--- gameSrc/specs/BaseBuildingsSpecs.js
+++ gameSrc/specs/BaseBuildingsSpecs.js
@@ -6,31 +6,53 @@ import BaseBuildings from "./../base/buildings/BaseBuildings.js"
 
 describe("Base Buildings", function(){
 
+  let houses
+  let farms
+  let lumberYards
+  let scrapYards
   let baseBuildings
 
   beforeEach(() => {
-    const houses = new Houses(6, 1)
-    const farms = new ProductionBuildingGroup(10, 100, 10)
-    const lumbarYards = new InputProductionBuildingGroup(3, 3, 50, 3)
-    const scrapYards = new InputProductionBuildingGroup(1.5, 3, 50, 3)
-    baseBuildings = new BaseBuildings(houses, farms, lumbarYards, scrapYards)
+    houses = new Houses(6, 1)
+    farms = new ProductionBuildingGroup(10, 100, 10)
+    lumberYards = new InputProductionBuildingGroup(3, 3, 50, 3)
+    scrapYards = new InputProductionBuildingGroup(1.5, 3, 50, 3)
+    baseBuildings = new BaseBuildings(houses, farms, lumberYards, scrapYards)
   })
 
   it("can initialise base builings", () => {
     assert.ok(baseBuildings)
     assert.ok(baseBuildings.houses)
     assert.ok(baseBuildings.farms)
-    assert.ok(baseBuildings.lumbarYards)
+    assert.ok(baseBuildings.lumberYards)
     assert.ok(baseBuildings.scrapYards)
+  })
+
+
+  it("cannot initialise with invalid objects", () => {
+    assert.throws(() => {
+      baseBuildings = new BaseBuildings(0, farms, lumberYards, scrapYards)
+    })
+    assert.throws(() => {
+      baseBuildings = new BaseBuildings(houses, 0, lumberYards, scrapYards)
+    })
+    assert.throws(() => {
+      baseBuildings = new BaseBuildings(houses, farms, 0, scrapYards)
+    })
+    assert.throws(() => {
+      baseBuildings = new BaseBuildings(houses, farms, lumberYards, 0)
+    })
   })
 
   it("can calculate overall size used", () => {
     baseBuildings.houses.add(10)
     baseBuildings.farms.add(10)
-    baseBuildings.lumbarYards.add(10)
+    baseBuildings.lumberYards.add(10)
     baseBuildings.scrapYards.add(10)
 
-    let expected = 10 + (10*10) + (10*3) + (10*3)
+    let expected = houses.usedSpace() + farms.usedSpace() +
+    lumberYards.usedSpace() + scrapYards.usedSpace()
+
     let result = baseBuildings.totalSpaceUsed()
     assert.strictEqual(result, expected)
   })
