57aff24f0886b73ea86edfa9f21edec638008033
James Cox <j-cox03@hotmail.com>
2017-11-19 19:20:52 +0000

    Wall: made methods count and produce

--- gameSrc/base/defence/Wall.js
+++ gameSrc/base/defence/Wall.js
@@ -1,9 +1,19 @@
 import EffortHandler from "./../../util/EffortHandler.js"
 
 class Wall{
-  constructor(){
+  constructor(productionPerEffort){
     this._materials = 0
     this.effort = new EffortHandler(Number.MAX_SAFE_INTEGER)
+    this._productionPerEffort = productionPerEffort
+  }
+
+  countMaterials(){
+    return this._materials
+  }
+
+  produce(){
+    this._materials += this.effort.currentEffort() * this._productionPerEffort
+    this.effort.reset()
   }
 
 }
--- gameSrc/specs/WallSpecs.js
+++ gameSrc/specs/WallSpecs.js
@@ -6,13 +6,44 @@ describe("Wall", function(){
   let wall
 
   beforeEach(() => {
-    wall = new Wall()
+    wall = new Wall(10)
   })
 
   it("can initialise Wall", () => {
     assert.ok(wall)
     assert.ok(wall.effort)
     assert.strictEqual(wall._materials, 0)
-    assert.strictEqual(wall.effort._capacity, Number.MAX_SAFE_INTEGER) 
+    assert.strictEqual(wall._productionPerEffort, 10)
+    assert.strictEqual(wall.effort._capacity, Number.MAX_SAFE_INTEGER)
+  })
+
+  it("can get materials count", () => {
+    let expected = 0
+    let result = wall.countMaterials()
+    assert.strictEqual(result, expected)
+  })
+
+  it("can produce (add materials to the wall)", () => {
+    wall.effort.add(10)
+
+    let expected = 10
+    let result = wall.effort.currentEffort()
+    assert.strictEqual(result, expected)
+
+    expected = 0
+    result = wall.countMaterials()
+    assert.strictEqual(result, expected)
+
+    wall.produce()
+
+    expected = 0
+    result = wall.effort.currentEffort()
+    assert.strictEqual(result, expected)
+
+    expected = 100
+    result = wall.countMaterials()
+    assert.strictEqual(result, expected)
+
+
   })
 })
