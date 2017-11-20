40387ed6fb4bdf2c2eaadea238e6166efff3402a
James Cox <j-cox03@hotmail.com>
2017-11-20 12:46:40 +0000

    Base: added tests for when constructing with options

--- gameSrc/base/Base.js
+++ gameSrc/base/Base.js
@@ -1,8 +1,10 @@
 import Population from "./population/Population.js"
+import TurnEffort from "./population/TurnEffort.js"
 
 class Base{
   constructor(options = {}){
     this.population = new Population(3, options.people)
+    this.effort = new TurnEffort(3, options.people)
   }
 }
 
