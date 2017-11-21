c822c45c6e6382a4b97c810302dc5e28e9960845
James Cox <j-cox03@hotmail.com>
2017-11-21 14:35:13 +0000

    PercentageDice: icreased max value to 1000 for more precise results

--- gameSrc/util/PercentageDice.js
+++ gameSrc/util/PercentageDice.js
@@ -1,11 +1,11 @@
 class PercentageDice{
   constructor(probability){
-    if(probability < 0 || probability > 100) throw new Error("Chance must be between 1 and 100 inclusivly")
-    this.probability = probability
+    if(probability < 0 || probability > 1000) throw new Error("Chance must be between 1 and 100 inclusivly")
+    this.probability = probability*10
   }
 
   chance(){
-    return Math.floor(Math.random() * ((100-1)+1) + 1) <= this.probability
+    return Math.floor(Math.random() * ((1000-1)+1) + 1) <= this.probability
   }
 }
 
