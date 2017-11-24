522190bfeba227c560268307d8612b9b976c5a76
James Cox <j-cox03@hotmail.com>
2017-11-24 14:39:22 +0000

    Tracker: added max and min values

--- gameSrc/specs/util/TrackerSpecs.js
+++ gameSrc/specs/util/TrackerSpecs.js
@@ -7,12 +7,6 @@ describe("Tracker", function(){
     assert.ok(new Tracker(0))
   })
 
-  it("cannot initialise with value less than 0", () => {
-    assert.throws(() => {
-      new Tracker(-1)
-    })
-  })
-
   it("can get the value", () => {
     const tracker = new Tracker(5)
     let expected = 5
@@ -31,6 +25,67 @@ describe("Tracker", function(){
     expected = 10
     result = tracker.val.get()
     assert.strictEqual(result, expected)
+  })
+
+  it("cannot increase the value with a negative number", () => {
+    const tracker = new Tracker(0)
+    assert.throws(() => {
+      tracker.val.inc(-1)
+    })
+    assert.throws(() => {
+      tracker.val.inc(-100)
+    })
+    assert.throws(() => {
+      tracker.val.inc(-10000)
+    })
+  })
+
+  it("can decrease the value", () => {
+    const tracker = new Tracker(5)
+    tracker.val.dec()
+    let expected = 4
+    let result = tracker.val.get()
+    assert.strictEqual(result, expected)
+
+    tracker.val.dec(4)
+    expected = 0
+    result = tracker.val.get()
+    assert.strictEqual(result, expected)
+  })
+
+  it("cannot increase the value with a negative number", () => {
+    const tracker = new Tracker(0)
+    assert.throws(() => {
+      tracker.val.dec(-1)
+    })
+    assert.throws(() => {
+      tracker.val.dec(-100)
+    })
+    assert.throws(() => {
+      tracker.val.dec(-10000)
+    })
+  })
+
+  it("can set a max and min value - on construction", () => {
+    const tracker = new Tracker(0, -5, 5)
+
+    tracker.val.inc(5)
+    let expected = 5
+    let result = tracker.val.get()
+    assert.strictEqual(result, expected)
+
+    assert.throws(() => {
+      tracker.val.inc(1)
+    })
+
+    tracker.val.dec(10)
+    expected = -5
+    result = tracker.val.get()
+    assert.strictEqual(result, expected)
+
+    assert.throws(() => {
+      tracker.val.dec(1)
+    })
   })
 
 })
--- gameSrc/util/Tracker.js
+++ gameSrc/util/Tracker.js
@@ -1,17 +1,38 @@
 
 class Tracker{
-  constructor(input){
-    if(input < 0) throw new Error("Values cannot be less than 0")
-
+  constructor(input, minValue, maxValue){
     this.val = function(){
       let value = input
+      let min = minValue
+      let max = maxValue
       return (
         {
           get: () => {return value},
           inc: (amount) => {
             amount = amount || 1
-            value += amount
-            return amount
+            if(amount < 0) throw new Error("Input cannot be negative: " + amount)
+            const result = amount + value
+            if(result > max) {
+              throw new Error(
+                `Cannot increase value above the max:
+                Max equals ${max} / Input equals ${amount}`
+              )
+            }
+            value = result
+            return value
+          },
+          dec: (amount) => {
+            amount = amount || 1
+            if(amount < 0) throw new Error("Input cannot be negative: " + amount)
+            const result = value - amount
+            if(result < min) {
+              throw new Error(
+                `Cannot decrease value below the min:
+                Min equals ${min} / Input equals ${amount}`
+              )
+            }
+            value = result
+            return value
           },
         }
       )
